d6bf09c9dcdf3c1ade6a7cb13adc9cefa2d35cc6
Yogesh Mane <yogeshmane41799@gmail.com>
2025-01-25 18:09:12 +0530

    final commit

--- restapi20_01/api/resttest.py
+++ restapi20_01/api/resttest.py
@@ -1,6 +1,6 @@
 import json
 import requests
-
+'''
 stud_data={'rollno':2,'name':'kishore','course':'oracle','fee':200}
 stud_json=json.dumps(stud_data)
 response=requests.post("http://localhost:8000/create/",data=stud_json)
@@ -13,8 +13,8 @@ print(response.json())
 
 response=requests.get("http://localhost:8000/get_all/")
 print(response.json())
-
-stud_data={'rollno':8,
+'''
+stud_data={'rollno':2,
            'fee':7000}
 stud_json=json.dumps(stud_data)
 response=requests.put("http://localhost:8000/update_stud/",data=stud_json)
